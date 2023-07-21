71b0f0de7f98114b7546b89aec194dd98f896b2e
Amar Kota <amar@amarkota.com>
2023-07-21 11:44:39 -0700

    added `__init__` and `main` methods

--- app/internal/stripe_module.py
+++ app/internal/stripe_module.py
@@ -1,38 +1,81 @@
 from pydantic import BaseModel
+from zoneinfo import ZoneInfo
 
+import os
+import calendar
 import datetime
+import dateutil.relativedelta
+import locale
+
+import stripe
 
 class RevenutStripe(BaseModel):
-    #region Properties
-    IsAuthorized: bool = False
-    Status:str | None = None
-    AccountID: str | None = None
-    AccountName: str | None = None
-    AccountIconURL: str | None = None
-    AuthorizationCode:str | None = None
-    TimezonePreference:str | None = None
-    DateToday:datetime.datetime = datetime.datetime.now()
-    DateDayStartCurrent:datetime.datetime = datetime.datetime.now()
-    DateDayEndCurrent:datetime.datetime = datetime.datetime.now()
-    DateMonthStartCurrent:datetime.datetime = datetime.datetime.now()
-    DateMonthEndCurrent:datetime.datetime = datetime.datetime.now()
-    DateMonthEndPrevious:datetime.datetime = datetime.datetime.now()
-    DateMonthToDateCurrent:datetime.datetime = datetime.datetime.now()
-    DateMonthStartPrevious:datetime.datetime = datetime.datetime.now()
-    DateMonthToDatePrevious:datetime.datetime = datetime.datetime.now()
-    VolumeGrossToday:float = 0
-    VolumeGrossMonthCurrent:float = 0
-    VolumeGrossMonthCurrentPercent:float = 0
-    VolumeGrossMonthPrevious:float = 0
-    VolumeGrossMonthToDatePrevious:float = 0
-    VolumeGrossMonthForecast:float = 0
-    VolumeGrossMonthOverMonthPercentChange:float = 0
-    VolumeGrossMonthToMonthPercentChange:float = 0
-    VolumePending:float = 0
-    VolumePendingPercent:float = 0
-    VolumeTrialing:float = 0
-    VolumeTrialingPercent:float = 0
-    CountPaymentsToday:int = 0
-    CountTrialingToday:int = 0
-    CountTrialingMonthCurrent:int = 0
-    #endregion
+	#region Properties
+	IsAuthorized: bool = False
+	Status:str | None = None
+	AccountID: str | None = None
+	AccountName: str | None = None
+	AccountIconURL: str | None = None
+	AuthorizationCode:str | None = None
+	TimezonePreference:str | None = None
+	DateToday:datetime.datetime = datetime.datetime.now()
+	DateDayStartCurrent:datetime.datetime = datetime.datetime.now()
+	DateDayEndCurrent:datetime.datetime = datetime.datetime.now()
+	DateMonthStartCurrent:datetime.datetime = datetime.datetime.now()
+	DateMonthEndCurrent:datetime.datetime = datetime.datetime.now()
+	DateMonthEndPrevious:datetime.datetime = datetime.datetime.now()
+	DateMonthToDateCurrent:datetime.datetime = datetime.datetime.now()
+	DateMonthStartPrevious:datetime.datetime = datetime.datetime.now()
+	DateMonthToDatePrevious:datetime.datetime = datetime.datetime.now()
+	VolumeGrossToday:float = 0
+	VolumeGrossMonthCurrent:float = 0
+	VolumeGrossMonthCurrentPercent:float = 0
+	VolumeGrossMonthPrevious:float = 0
+	VolumeGrossMonthToDatePrevious:float = 0
+	VolumeGrossMonthForecast:float = 0
+	VolumeGrossMonthOverMonthPercentChange:float = 0
+	VolumeGrossMonthToMonthPercentChange:float = 0
+	VolumePending:float = 0
+	VolumePendingPercent:float = 0
+	VolumeTrialing:float = 0
+	VolumeTrialingPercent:float = 0
+	CountPaymentsToday:int = 0
+	CountTrialingToday:int = 0
+	CountTrialingMonthCurrent:int = 0
+	#endregion
+
+	def __init__(self, *a, **kw):
+		super().__init__(*a, **kw)
+
+		stripe.api_key = os.getenv('API_KEY_STRIPE')
+		stripe.log = 'info'
+
+	def init_locale(self) -> None:
+		locale.setlocale(locale.LC_ALL, '')
+		self.DateToday = self.DateToday.astimezone(ZoneInfo(self.TimezonePreference)) if self.TimezonePreference else datetime.datetime.now()
+		self.DateDayStartCurrent = self.DateToday.replace(hour=0, minute=0, second=0, microsecond=0)
+		self.DateDayEndCurrent = self.DateToday.replace(hour=23, minute=59, second=59, microsecond=999)
+		self.DateMonthStartCurrent = self.DateToday.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+		self.DateMonthEndCurrent = self.DateToday.replace(day=calendar.monthrange(self.DateToday.year, self.DateToday.month)[1], hour=23, minute=59, second=59, microsecond=999)
+		self.DateMonthToDateCurrent = self.DateToday.replace(day=self.DateToday.day, hour=23, minute=59, second=59, microsecond=999)
+
+		self.DateMonthStartPrevious = self.DateMonthStartCurrent + dateutil.relativedelta.relativedelta(months=-1)
+		self.DateMonthEndPrevious = self.DateMonthEndCurrent + dateutil.relativedelta.relativedelta(months=-1)
+		self.DateMonthToDatePrevious = self.DateMonthStartPrevious.replace(day=self.DateToday.day, hour=23, minute=59, second=59, microsecond=999)
+
+
+def main():
+	print("Calling Stripe API...")
+	mystripe = RevenutStripe()
+
+	print(f"""
+		Status: {mystripe.Status}
+		VolumeGrossToday: {locale.currency(mystripe.VolumeGrossToday)}
+		VolumeGrossMonthCurrent: {locale.currency(mystripe.VolumeGrossMonthCurrent)}
+		VolumePending: {locale.currency(mystripe.VolumePending)}
+		VolumeGrossMonthTrialing: {locale.currency(mystripe.VolumeTrialing)}
+		VolumeGrossMonthForecast: {locale.currency(mystripe.VolumeGrossMonthForecast)}
+	""")
+
+if __name__ == '__main__':
+	main()
